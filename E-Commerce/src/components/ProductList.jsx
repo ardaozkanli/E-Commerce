4cff0d6281f89c1a21162898d5a474937ec86de0
Arda Özkanlı <ardaozkanli@hotmail.com>
2025-04-10 16:35:59 +0300

    Revert "312"
    This reverts commit 876c6c05c642459e983bb626602f1a2c4924ae98.

--- E-Commerce/src/components/ProductList.jsx
+++ E-Commerce/src/components/ProductList.jsx
@@ -1,50 +1,24 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../redux/slices/productSlices";
-import useScreenShotDetect from "../hooks/useScreenShotDetect";
 import Product from "./Product";
-
 function ProductList() {
   const dispatch = useDispatch();
   const { products } = useSelector((store) => store.product);
 
-  // Screenshot algılama hook'unu burada çağır
-  useScreenShotDetect(() => {
-    alert("Ekran görüntüsü alındı!");
-  });
-
   useEffect(() => {
     dispatch(getAllProducts());
-  }, [dispatch]);
+  }, []);
+  return <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "10px", flexDirection: "column", }}>
+    <h2>PRODUCTS</h2>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "10px", flexDirection: "row", flexWrap: "wrap" }}>
+      {
+        products && products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "10px",
-        flexDirection: "column",
-      }}
-    >
-      <h2>PRODUCTS</h2>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: "10px",
-          flexDirection: "row",
-          flexWrap: "wrap",
-        }}
-      >
-        {products &&
-          products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
-      </div>
-    </div>
-  );
+      }</div>;
+  </div>
 }
 
 export default ProductList;
